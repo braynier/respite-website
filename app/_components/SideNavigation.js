1731f3f55c10fe72bf12ee50a28f9088b91d5272
Mindaugas <mindaugas.jasiunas97@gmail.com>
2024-11-04 18:12:00 +0200

    fixed side navigation bug

--- app/_components/SideNavigation.js
+++ app/_components/SideNavigation.js
@@ -30,12 +30,18 @@ function SideNavigation() {
   const pathname = usePathname();
 
   return (
-    <nav className="border-r border-primary-900">
-      <ul className="flex h-full flex-col gap-2 text-lg md:flex-row lg:flex-col">
+    <nav className="z-10 h-full border-r border-primary-900">
+      {" "}
+      {/* Added z-10 */}
+      <ul className="relative z-10 flex flex-col gap-2 text-lg">
+        {" "}
+        {/* Added z-10 and relative */}
         {navLinks.map((link) => (
-          <li key={link.name}>
+          <li key={link.name} className="flex-grow">
             <Link
-              className={`flex items-center gap-4 px-5 py-3 font-semibold text-primary-200 transition-colors hover:bg-primary-900 hover:text-primary-100 ${pathname === link.href ? "bg-primary-900" : ""}`}
+              className={`flex items-center gap-4 px-5 py-3 font-semibold text-primary-200 transition-colors hover:bg-primary-900 hover:text-primary-100 ${
+                pathname === link.href ? "bg-primary-900" : ""
+              }`}
               href={link.href}
             >
               {link.icon}
@@ -43,8 +49,7 @@ function SideNavigation() {
             </Link>
           </li>
         ))}
-
-        <li className="lg:mt-auto">
+        <li className="mt-auto">
           <SignOutButton />
         </li>
       </ul>
--- app/account/layout.js
+++ app/account/layout.js
@@ -1,4 +1,3 @@
-import Navigation from "../_components/Navigation";
 import SideNavigation from "../_components/SideNavigation";
 
 export default function Layout({ children }) {
